37b95fe5f926597fccf6e6c0f0991250f6cb2c4b
Adam Glachan <adam.glachan@gmail.com>
2024-10-23 13:33:36 +1000

    Added media queries for smaller screens.

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuButton = document.getElementById("menuButton");
+const header = document.querySelector("header");
+
+menuButton.addEventListener("click", toggleMenu);
+const navLinks = document.querySelectorAll("#pageNavigation a");
+
+function toggleMenu() {}
